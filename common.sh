7d58fe238922e408f9a0dfbe6832f6961144e5d0
Ankit Shubham <ankitshubham97@gmail.com>
2019-08-05 21:57:09 +0530

    Move repetitive code to common.sh

---
+++ common.sh
@@ -0,0 +1,22 @@
+if [[ -d "build" && ! -L "build" ]]; then
+    cd build
+    if [[ -d "src" && ! -L "src" ]]; then
+        rm -rf ./src/com/mani/compiler/resources/*
+        cd ../
+    else
+        cd ../
+        rm -rf ./build
+        git clone https://github.com/Mani-Language/Mani build
+    fi
+else
+    git clone https://github.com/Mani-Language/Mani build
+fi
+
+cd build
+gradle clean
+rm -rf ./src/com/mani/compiler/resources/*
+cp -r ../src/* ./src/com/mani/compiler/resources/
+gradle buildCompiler
+clear
+# Done to set the pwd to this dir
+cd ../
